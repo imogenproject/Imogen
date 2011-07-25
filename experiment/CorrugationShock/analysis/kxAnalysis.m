3b3e4bd863ca741f6b5b319827879394595984f7
Erik Keever <ekeever1@uoregon.edu>
2011-07-25 16:36:11 -0700

    Makes kx and omega plot routines work.

--- experiment/CorrugationShock/analysis/kxAnalysis.m
+++ experiment/CorrugationShock/analysis/kxAnalysis.m
@@ -6,18 +6,18 @@ hold off;
 
 subplot(1,2,1);
 
-plot(imag(analysis.post.drhoKx)./analysis.kyWavenums,'r','DisplayName','drho post');
+plot(imag(analysis.post.drhoKx)./analysis.kyValues,'r','DisplayName','drho post');
 hold on;
-plot(imag(analysis.post.dvxKx) ./analysis.kyWavenums,'g-','DisplayName','dvx post');
-plot(imag(analysis.post.dvyKx) ./analysis.kyWavenums,'b-','DisplayName','dvy post');
-plot(imag(analysis.post.dbyKx) ./analysis.kyWavenums,'c-','DisplayName','dby post');
+plot(imag(analysis.post.dvxKx) ./analysis.kyValues,'g-','DisplayName','dvx post');
+plot(imag(analysis.post.dvyKx) ./analysis.kyValues,'b-','DisplayName','dvy post');
+plot(imag(analysis.post.dbyKx) ./analysis.kyValues,'c-','DisplayName','dby post');
 
-plot(imag(analysis.pre.drhoKx)./analysis.kyWavenums,'r-.','DisplayName','drho pre');
-plot(imag(analysis.pre.dvxKx) ./analysis.kyWavenums,'g-.','DisplayName','dvx pre');
-plot(imag(analysis.pre.dvyKx) ./analysis.kyWavenums,'b-.','DisplayName','dvy pre');
-plot(imag(analysis.pre.dbyKx) ./analysis.kyWavenums,'c-.','DisplayName','dby pre');
-plot(real(analysis.linear.omega(1:analysis.nModes(1)))./analysis.kyWavenums','k');
-plot(0*numel(analysis.kyWavenums),'k-');
+plot(imag(analysis.pre.drhoKx)./analysis.kyValues,'r-.','DisplayName','drho pre');
+plot(imag(analysis.pre.dvxKx) ./analysis.kyValues,'g-.','DisplayName','dvx pre');
+plot(imag(analysis.pre.dvyKx) ./analysis.kyValues,'b-.','DisplayName','dvy pre');
+plot(imag(analysis.pre.dbyKx) ./analysis.kyValues,'c-.','DisplayName','dby pre');
+%plot(real(analysis.linear.omega(1:analysis.nModes(1)))./analysis.kyValues','k');
+plot(0*numel(analysis.kyValues),'k-');
 
 xlabel('Mode number','fontsize',fsize);
 ylabel('imag(kx) / Ky','fontsize',fsize);
@@ -28,18 +28,18 @@ grid on
 
 subplot(1,2,2);
 
-plot(real(analysis.post.drhoKx)./analysis.kyWavenums,'r','DisplayName','drho post');
+plot(real(analysis.post.drhoKx)./analysis.kyValues,'r','DisplayName','drho post');
 hold on;
-plot(real(analysis.post.dvxKx) ./analysis.kyWavenums,'g-','DisplayName','dvx post');
-plot(real(analysis.post.dvyKx) ./analysis.kyWavenums,'b-','DisplayName','dvy post');
-plot(real(analysis.post.dbyKx) ./analysis.kyWavenums,'c-','DisplayName','dby post');
+plot(real(analysis.post.dvxKx) ./analysis.kyValues,'g-','DisplayName','dvx post');
+plot(real(analysis.post.dvyKx) ./analysis.kyValues,'b-','DisplayName','dvy post');
+plot(real(analysis.post.dbyKx) ./analysis.kyValues,'c-','DisplayName','dby post');
 
-plot(real(analysis.pre.drhoKx)./analysis.kyWavenums,'r-.','DisplayName','drho pre');
-plot(real(analysis.pre.dvxKx) ./analysis.kyWavenums,'g-.','DisplayName','dvx pre');
-plot(real(analysis.pre.dvyKx) ./analysis.kyWavenums,'b-.','DisplayName','dvy pre');
-plot(real(analysis.pre.dbyKx) ./analysis.kyWavenums,'c-.','DisplayName','dby pre');
-plot(real(analysis.linear.omega(1:analysis.nModes(1)))./analysis.kyWavenums','k');
-plot(0*numel(analysis.kyWavenums),'k-');
+plot(real(analysis.pre.drhoKx)./analysis.kyValues,'r-.','DisplayName','drho pre');
+plot(real(analysis.pre.dvxKx) ./analysis.kyValues,'g-.','DisplayName','dvx pre');
+plot(real(analysis.pre.dvyKx) ./analysis.kyValues,'b-.','DisplayName','dvy pre');
+plot(real(analysis.pre.dbyKx) ./analysis.kyValues,'c-.','DisplayName','dby pre');
+%plot(real(analysis.linear.omega(1:analysis.nModes(1)))./analysis.kyValues','k');
+plot(0*numel(analysis.kyValues),'k-');
 
 grid on;
 
--- experiment/CorrugationShock/analysis/omegaAnalysis.m
+++ experiment/CorrugationShock/analysis/omegaAnalysis.m
@@ -8,18 +8,18 @@ hold off;
 
 subplot(1,2,1);
 
-plot(vinv*imag(analysis.omega_fromdrho2)./analysis.kyWavenums,'r','DisplayName','drho post');
+plot(vinv*imag(analysis.omega.fromdrho2)./analysis.kyValues,'r','DisplayName','drho post');
 hold on;
-plot(vinv*imag(analysis.omega_fromdrho2)./analysis.kyWavenums,'r-','DisplayName','drho post');
-plot(vinv*imag(analysis.omega_fromdvx2) ./analysis.kyWavenums,'g-','DisplayName','dvx post');
-plot(vinv*imag(analysis.omega_fromdvy2) ./analysis.kyWavenums,'b-','DisplayName','dvy post');
-plot(vinv*imag(analysis.omega_fromdrho1)./analysis.kyWavenums,'r-.','DisplayName','drho pre');
-plot(vinv*imag(analysis.omega_fromdvx1) ./analysis.kyWavenums,'g-.','DisplayName','dvx pre');
-plot(vinv*imag(analysis.omega_fromdvy1) ./analysis.kyWavenums,'b-.','DisplayName','dvy pre');
-plot(vinv*imag(analysis.omega_fromdby2) ./analysis.kyWavenums,'c-','DisplayName','dby post');
-plot(vinv*imag(analysis.omega_fromdby1) ./analysis.kyWavenums,'c-.','DisplayName','dby pre');
-plot(vinv*imag(analysis.linear.omega(1:analysis.nModes(1)))./analysis.kyWavenums','k','DisplayName','eta');
-plot(0*numel(analysis.kyWavenums),'k-');
+plot(vinv*imag(analysis.omega.fromdrho2)./analysis.kyValues,'r-','DisplayName','drho post');
+plot(vinv*imag(analysis.omega.fromdvx2) ./analysis.kyValues,'g-','DisplayName','dvx post');
+plot(vinv*imag(analysis.omega.fromdvy2) ./analysis.kyValues,'b-','DisplayName','dvy post');
+plot(vinv*imag(analysis.omega.fromdrho1)./analysis.kyValues,'r-.','DisplayName','drho pre');
+plot(vinv*imag(analysis.omega.fromdvx1) ./analysis.kyValues,'g-.','DisplayName','dvx pre');
+plot(vinv*imag(analysis.omega.fromdvy1) ./analysis.kyValues,'b-.','DisplayName','dvy pre');
+plot(vinv*imag(analysis.omega.fromdby2) ./analysis.kyValues,'c-','DisplayName','dby post');
+plot(vinv*imag(analysis.omega.fromdby1) ./analysis.kyValues,'c-.','DisplayName','dby pre');
+plot(vinv*imag(analysis.omega.front(1:analysis.nModes(1)))./analysis.kyValues','k','DisplayName','eta');
+plot(0*numel(analysis.kyValues),'k-');
 
 xlabel('Mode number','fontsize',fsize);
 ylabel('imag(omega) / Ky Vin','fontsize',fsize);
@@ -30,18 +30,18 @@ grid on
 
 subplot(1,2,2);
 
-plot(vinv*real(analysis.omega_fromdrho2)./analysis.kyWavenums,'r','DisplayName','drho post');
+plot(vinv*real(analysis.omega.fromdrho2)./analysis.kyValues,'r','DisplayName','drho post');
 hold on;
-plot(vinv*real(analysis.omega_fromdrho2)./analysis.kyWavenums,'r-','DisplayName','drho post');
-plot(vinv*real(analysis.omega_fromdvx2) ./analysis.kyWavenums,'g-','DisplayName','dvx post');
-plot(vinv*real(analysis.omega_fromdvy2) ./analysis.kyWavenums,'b-','DisplayName','dvy post');
-plot(vinv*real(analysis.omega_fromdrho1)./analysis.kyWavenums,'r-.','DisplayName','drho pre');
-plot(vinv*real(analysis.omega_fromdvx1) ./analysis.kyWavenums,'g-.','DisplayName','dvx pre');
-plot(vinv*real(analysis.omega_fromdvy1) ./analysis.kyWavenums,'b-.','DisplayName','dvy pre');
-plot(vinv*real(analysis.omega_fromdby2) ./analysis.kyWavenums,'c-','DisplayName','dby post');
-plot(vinv*real(analysis.omega_fromdby1) ./analysis.kyWavenums,'c-.','DisplayName','dby pre');
-plot(vinv*real(analysis.linear.omega(1:analysis.nModes(1)))./analysis.kyWavenums','k');
-plot(0*numel(analysis.kyWavenums),'k-');
+plot(vinv*real(analysis.omega.fromdrho2)./analysis.kyValues,'r-','DisplayName','drho post');
+plot(vinv*real(analysis.omega.fromdvx2) ./analysis.kyValues,'g-','DisplayName','dvx post');
+plot(vinv*real(analysis.omega.fromdvy2) ./analysis.kyValues,'b-','DisplayName','dvy post');
+plot(vinv*real(analysis.omega.fromdrho1)./analysis.kyValues,'r-.','DisplayName','drho pre');
+plot(vinv*real(analysis.omega.fromdvx1) ./analysis.kyValues,'g-.','DisplayName','dvx pre');
+plot(vinv*real(analysis.omega.fromdvy1) ./analysis.kyValues,'b-.','DisplayName','dvy pre');
+plot(vinv*real(analysis.omega.fromdby2) ./analysis.kyValues,'c-','DisplayName','dby post');
+plot(vinv*real(analysis.omega.fromdby1) ./analysis.kyValues,'c-.','DisplayName','dby pre');
+plot(vinv*real(analysis.omega.front(1:analysis.nModes(1)))./analysis.kyValues','k');
+plot(0*numel(analysis.kyValues),'k-');
 
 grid on;
 
