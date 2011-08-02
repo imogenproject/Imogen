3ef5cb2237797ee0432bd50f0a5c002d40a6c664
Erik Keever <ekeever1@uoregon.edu>
2011-08-02 08:11:43 -0700

    omega and kx plotter routines display all variables correctly and draw the f=0 line right.

--- experiment/CorrugationShock/analysis/kxAnalysis.m
+++ experiment/CorrugationShock/analysis/kxAnalysis.m
@@ -10,14 +10,16 @@ plot(-imag(analysis.post.drhoKx)./analysis.kyValues,'r','DisplayName','drho post
 hold on;
 plot(-imag(analysis.post.dvxKx) ./analysis.kyValues,'g-','DisplayName','dvx post');
 plot(-imag(analysis.post.dvyKx) ./analysis.kyValues,'b-','DisplayName','dvy post');
+plot(-imag(analysis.post.dbxKx) ./analysis.kyValues,'m-','DisplayName','dbx post');
 plot(-imag(analysis.post.dbyKx) ./analysis.kyValues,'c-','DisplayName','dby post');
 
 plot(-imag(analysis.pre.drhoKx)./analysis.kyValues,'r-.','DisplayName','drho pre');
 plot(-imag(analysis.pre.dvxKx) ./analysis.kyValues,'g-.','DisplayName','dvx pre');
 plot(-imag(analysis.pre.dvyKx) ./analysis.kyValues,'b-.','DisplayName','dvy pre');
+plot(-imag(analysis.pre.dbxKx) ./analysis.kyValues,'m-.','DisplayName','dbx pre');
 plot(-imag(analysis.pre.dbyKx) ./analysis.kyValues,'c-.','DisplayName','dby pre');
-%plot(real(analysis.linear.omega(1:analysis.nModes(1)))./analysis.kyValues','k');
-plot(0*numel(analysis.kyValues),'k-');
+
+plot(1:analysis.nModes(1), zeros(size(analysis.kyValues)),'k-','DisplayName','');
 
 xlabel('Mode number','fontsize',fsize);
 ylabel('imag(kx) / Ky','fontsize',fsize);
@@ -32,15 +34,16 @@ plot(real(analysis.post.drhoKx)./analysis.kyValues,'r','DisplayName','drho post'
 hold on;
 plot(real(analysis.post.dvxKx) ./analysis.kyValues,'g-','DisplayName','dvx post');
 plot(real(analysis.post.dvyKx) ./analysis.kyValues,'b-','DisplayName','dvy post');
+plot(real(analysis.post.dbxKx) ./analysis.kyValues,'m-','DisplayName','dbx post');
 plot(real(analysis.post.dbyKx) ./analysis.kyValues,'c-','DisplayName','dby post');
 
 plot(real(analysis.pre.drhoKx)./analysis.kyValues,'r-.','DisplayName','drho pre');
 plot(real(analysis.pre.dvxKx) ./analysis.kyValues,'g-.','DisplayName','dvx pre');
 plot(real(analysis.pre.dvyKx) ./analysis.kyValues,'b-.','DisplayName','dvy pre');
+plot(real(analysis.pre.dbxKx) ./analysis.kyValues,'m-.','DisplayName','dbx pre');
 plot(real(analysis.pre.dbyKx) ./analysis.kyValues,'c-.','DisplayName','dby pre');
-%plot(real(analysis.linear.omega(1:analysis.nModes(1)))./analysis.kyValues','k');
-plot(0*numel(analysis.kyValues),'k-');
 
+plot(1:analysis.nModes(1), zeros(size(analysis.kyValues)),'k-','DisplayName','');
 grid on;
 
 xlabel('Mode number','fontsize',fsize);
--- experiment/CorrugationShock/analysis/omegaAnalysis.m
+++ experiment/CorrugationShock/analysis/omegaAnalysis.m
@@ -13,13 +13,18 @@ hold on;
 plot(vinv*imag(analysis.omega.fromdrho2)./analysis.kyValues,'r-','DisplayName','drho post');
 plot(vinv*imag(analysis.omega.fromdvx2) ./analysis.kyValues,'g-','DisplayName','dvx post');
 plot(vinv*imag(analysis.omega.fromdvy2) ./analysis.kyValues,'b-','DisplayName','dvy post');
+plot(vinv*imag(analysis.omega.fromdbx2) ./analysis.kyValues,'m-','DisplayName','dbx post');
+plot(vinv*imag(analysis.omega.fromdby2) ./analysis.kyValues,'c-','DisplayName','dby post');
+
 plot(vinv*imag(analysis.omega.fromdrho1)./analysis.kyValues,'r-.','DisplayName','drho pre');
 plot(vinv*imag(analysis.omega.fromdvx1) ./analysis.kyValues,'g-.','DisplayName','dvx pre');
 plot(vinv*imag(analysis.omega.fromdvy1) ./analysis.kyValues,'b-.','DisplayName','dvy pre');
-plot(vinv*imag(analysis.omega.fromdby2) ./analysis.kyValues,'c-','DisplayName','dby post');
+plot(vinv*imag(analysis.omega.fromdbx1) ./analysis.kyValues,'m-.','DisplayName','dbx pre')
 plot(vinv*imag(analysis.omega.fromdby1) ./analysis.kyValues,'c-.','DisplayName','dby pre');
+
 plot(vinv*imag(analysis.omega.front(1:analysis.nModes(1)))./analysis.kyValues','k','DisplayName','eta');
-plot(0*numel(analysis.kyValues),'k-');
+
+plot(1:analysis.nModes(1), zeros(size(analysis.kyValues)),'k-','DisplayName','');
 
 xlabel('Mode number','fontsize',fsize);
 ylabel('imag(omega) / Ky Vin','fontsize',fsize);
@@ -29,19 +34,23 @@ title('Fits of im(omega*) using multiple perturbed quantities','fontsize',fsize)
 grid on
 
 subplot(1,2,2);
-
 plot(vinv*real(analysis.omega.fromdrho2)./analysis.kyValues,'r','DisplayName','drho post');
 hold on;
 plot(vinv*real(analysis.omega.fromdrho2)./analysis.kyValues,'r-','DisplayName','drho post');
 plot(vinv*real(analysis.omega.fromdvx2) ./analysis.kyValues,'g-','DisplayName','dvx post');
 plot(vinv*real(analysis.omega.fromdvy2) ./analysis.kyValues,'b-','DisplayName','dvy post');
+plot(vinv*real(analysis.omega.fromdbx2) ./analysis.kyValues,'m-','DisplayName','dbx post');
+plot(vinv*real(analysis.omega.fromdby2) ./analysis.kyValues,'c-','DisplayName','dby post');
+
 plot(vinv*real(analysis.omega.fromdrho1)./analysis.kyValues,'r-.','DisplayName','drho pre');
 plot(vinv*real(analysis.omega.fromdvx1) ./analysis.kyValues,'g-.','DisplayName','dvx pre');
 plot(vinv*real(analysis.omega.fromdvy1) ./analysis.kyValues,'b-.','DisplayName','dvy pre');
-plot(vinv*real(analysis.omega.fromdby2) ./analysis.kyValues,'c-','DisplayName','dby post');
+plot(vinv*real(analysis.omega.fromdbx1) ./analysis.kyValues,'m-.','DisplayName','dbx pre')
 plot(vinv*real(analysis.omega.fromdby1) ./analysis.kyValues,'c-.','DisplayName','dby pre');
-plot(vinv*real(analysis.omega.front(1:analysis.nModes(1)))./analysis.kyValues','k');
-plot(0*numel(analysis.kyValues),'k-');
+
+plot(vinv*real(analysis.omega.front(1:analysis.nModes(1)))./analysis.kyValues','k','DisplayName','eta');
+
+plot(1:analysis.nModes(1), zeros(size(analysis.kyValues)),'k-','DisplayName','');
 
 grid on;
 
